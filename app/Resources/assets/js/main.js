ef12a24a297a5a29b8768ab10b4ba784fd0becc9
gustavo <gustavo@mail.com>
2017-09-06 11:22:28 -0300

    Aplicando estilos fomulario login

--- app/Resources/assets/js/main.js
+++ app/Resources/assets/js/main.js
@@ -3,14 +3,6 @@ var $ = require('jquery');
 //require('bootstrap-sass');
 require('materialize-css');
 
-// var req = require('redirectjs');
-
-// $("#redirectjs").click(function () {
-//   req.redirectTo('https://github.com/nioperas06/redirectjs');
-// })
-
-
-  $(document).ready(function(){
-    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
-    $('.modal').modal();
-  });
+$(document).ready(function() {
+	Materialize.updateTextFields();
+});
