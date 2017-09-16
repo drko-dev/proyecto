22b7096092165bccde57bed940a1322988e63504
gustavo <gustavo@mail.com>
2017-09-16 16:01:55 -0300

    probando materialize

--- app/Resources/assets/js/main.js
+++ app/Resources/assets/js/main.js
@@ -1,3 +1,16 @@
 var $ = require('jquery');
 
 require('materialize-css');
+
+
+$('.button-collapse').sideNav(
+      // {
+      	//menuWidth: 300, // Default is 300
+      //edge: 'right', // Choose the horizontal origin
+      //closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      //draggable: true, // Choose whether you can drag to open on touch screens,
+      // A function to be called when sideNav is opened
+       // A function to be called when sideNav is closed
+//      onOpen: function(el) {  }, 
+  //    onClose: function(el) {  },}
+ );
